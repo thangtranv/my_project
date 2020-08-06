21b394ad6c263a630f96d9b1f37cc2920cb9f3fd
thangtranv <thangtv.yec@gmail.com>
2020-08-06 20:58:39 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    else:
+        for i in range(2,int(n**1/2)):
+            if n % i == 0:
+                return False
+    return True
+
+n = int(input())
+print(is_prime(n))
